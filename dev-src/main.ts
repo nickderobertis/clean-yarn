284c95ff52c6131217c1d2548c314fde7b620070
Nick DeRobertis <whoopnip@gmail.com>
2022-06-28 18:33:33 -0400

    chore: FIx some issues in the unused dev app

--- dev-src/main.ts
+++ dev-src/main.ts
@@ -1,11 +1,8 @@
-import { myFunction } from '../src';
-import './style.css';
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-const content = myFunction();
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
-  <h1>${content}</h1>
+  <h1>Placeholder content</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
