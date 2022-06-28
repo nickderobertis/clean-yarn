064fac808152c1f77f3ad7f42e15c93773ba2ae7
Nick DeRobertis <whoopnip@gmail.com>
2022-06-28 15:47:53 -0400

    chore: Restructure creating projects into utility functions

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,10 @@
 import { resolve } from "path";
-import { existsSync, mkdirSync } from "fs";
 import { describe, it, beforeEach, afterEach, expect, vi } from "vitest";
-import {
-  createPackageJson,
-  createPackageJsonAndInstall,
-  yarnInstall,
-} from "../testutils/ext-yarn";
 import { cleanUpTempDir, createTempDir } from "../testutils/temp-dir";
-import { createYarnRCYaml } from "../testutils/yarn-utils";
+import {
+  createMonorepoProject,
+  createNonMonorepoProject,
+} from "../testutils/project";
 
 let tempDir: string;
 
@@ -21,62 +18,10 @@ describe("index", () => {
   });
 
   it("creates a non-monorepo", async () => {
-    await createYarnRCYaml({ nodeLinker: "node-modules" }, tempDir);
-    await createPackageJsonAndInstall(
-      { dependencies: { lodash: "*" } },
-      { cwd: tempDir }
-    );
-    const nodeModulesPath = resolve(tempDir, "node_modules");
-    expect(existsSync(nodeModulesPath)).toBe(true);
-    const yarnLockPath = resolve(tempDir, "yarn.lock");
-    expect(existsSync(yarnLockPath)).toBe(true);
+    await createNonMonorepoProject(tempDir);
   });
 
   it("creates a monorepo", async () => {
-    await createYarnRCYaml({ nodeLinker: "node-modules" }, tempDir);
-    // Create apps and packages folders in the tempDir
-    const appPath = resolve(tempDir, "apps/my-app");
-    const packagePath = resolve(tempDir, "packages/my-package");
-    mkdirSync(appPath, { recursive: true });
-    mkdirSync(packagePath, { recursive: true });
-
-    await createPackageJson(
-      {
-        workspaces: { packages: ["packages/*", "apps/*"] },
-        dependencies: { lodash: "*" },
-        name: "monorepo",
-      },
-      { cwd: tempDir }
-    );
-    await createPackageJson(
-      {
-        dependencies: { lodash: "*", typescript: "*", chalk: "*" },
-        name: "my-app",
-        installConfig: { hoistingLimits: "workspaces" },
-      },
-      { cwd: appPath }
-    );
-    await createPackageJson(
-      {
-        dependencies: { typescript: "*", request: "*" },
-        name: "my-package",
-        installConfig: { hoistingLimits: "workspaces" },
-      },
-      { cwd: packagePath }
-    );
-    const result = await yarnInstall(tempDir);
-    console.log(result);
-
-    // Check that root node_modules and yarn.lock exist
-    const nodeModulesPath = resolve(tempDir, "node_modules");
-    expect(existsSync(nodeModulesPath)).toBe(true);
-    const yarnLockPath = resolve(tempDir, "yarn.lock");
-    expect(existsSync(yarnLockPath)).toBe(true);
-
-    // Check that node_modules folders exist in apps and package folders
-    const appNodeModulesPath = resolve(appPath, "node_modules");
-    expect(existsSync(appNodeModulesPath)).toBe(true);
-    const packageNodeModulesPath = resolve(packagePath, "node_modules");
-    expect(existsSync(packageNodeModulesPath)).toBe(true);
+    await createMonorepoProject(tempDir);
   }, 10000);
 });
